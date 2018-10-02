1bc4ff0fb5e56dfa3f682634e70643fbf3ba6375
Tobias Guimarães <tobiasguimaraes@Tobiass-Mac-mini.local>
2018-10-02 14:51:30 -0300

    preview

---
+++ app/src/main/java/me/tguima/appeatit/ui/main/MainFragment.kt
@@ -0,0 +1,16 @@
+package me.tguima.appeatit.ui.main
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import me.tguima.appeatit.R
+
+
+class MainFragment : Fragment() {
+
+}
